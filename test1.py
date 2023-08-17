a85630b05e220a33312c4c5a52d953aba1ae488f
Micheal-Gregor <63747644+Micheal-Gregor@users.noreply.github.com>
2023-08-17 15:49:21 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,15 +2,15 @@ import os
 import openai
 import streamlit as st
 
-st.header(“Autoresponse generator to any reviews”)
-reviews=st.text_area(“Copy Paste any customer Review”)
-button= st.button(“Autogenerate Response”)
+st.header("Autoresponse generator to any reviews")
+reviews=st.text_area("Copy Paste any customer review")
+button= st.button("Autogenerate Response")
 
 def gen_auto_response(reviews):
-openai.api_key = “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”
+openai.api_key = "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"
 response = openai.ChatCompletion.create(
 model="text-davinci-003",
-prompt=”Auto response generator \n\n”Review: {reviews} \n\nReply: \n”,
+prompt="Auto response generator \n\n”Review: {reviews} \n\nReply: \n",
 temperature=0.7,
 max_tokens=256,
 top_p=1,
@@ -20,6 +20,6 @@ presence_penalty=0
 print(response)
 return response.choices[0].text
 If reviews and button:
-	With st.spinner(“………………….Generating Autoresponse to your review………………..”):
+	With st.spinner(".....Generating Autoresponse to your review....."):
 	Reply=gen_auto_response(reviews)
 st.write(reply)
