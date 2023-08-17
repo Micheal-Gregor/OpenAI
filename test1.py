e103340427913c412d9dfaefc2597ec36fcc6f04
Micheal-Gregor <63747644+Micheal-Gregor@users.noreply.github.com>
2023-08-17 16:00:07 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,21 +7,21 @@ reviews=st.text_area("Copy Paste any customer review")
 button= st.button("Autogenerate Response")
 
 def gen_auto_response(reviews):
-    openai.api_key = "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"
+	openai.api_key = "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"
     
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Auto response generator \n\nReview: {reviews} \n\nReply: \n ",
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    print(response)
-    return response.choices[0].text
+	response = openai.Completion.create(
+		model="text-davinci-003",
+        	prompt="Auto response generator \n\nReview: {reviews} \n\nReply: \n ",
+        	temperature=0.7,
+        	max_tokens=256,
+        	top_p=1,
+        	frequency_penalty=0,
+        	presence_penalty=0
+	)
+	print(response)
+	return response.choices[0].text
 
 if reviews and button:
 	with st.spinner(".....Generating Autoresponse to your review....."):
-        reply=gen_auto_response(reviews)
-        st.write(reply)
+	reply=gen_auto_response(reviews)
+	st.write(reply)
