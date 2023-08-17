536858796775e54bd8bb3ec2c6f8991aa9d81968
Micheal-Gregor <63747644+Micheal-Gregor@users.noreply.github.com>
2023-08-17 15:55:21 -0700

    Add files via upload

--- test1.py
+++ test1.py
@@ -7,19 +7,21 @@ reviews=st.text_area("Copy Paste any customer review")
 button= st.button("Autogenerate Response")
 
 def gen_auto_response(reviews):
-openai.api_key = "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"
-response = openai.ChatCompletion.create(
-model="text-davinci-003",
-prompt="Auto response generator \n\n”Review: {reviews} \n\nReply: \n",
-temperature=0.7,
-max_tokens=256,
-top_p=1,
-frequency_penalty=0,
-presence_penalty=0
-)
-print(response)
-return response.choices[0].text
+    openai.api_key = "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"
+    
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Auto response generator \n\nReview: {reviews} \n\nReply: \n ",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    print(response)
+    return response.choices[0].text
+
 If reviews and button:
 	With st.spinner(".....Generating Autoresponse to your review....."):
-	Reply=gen_auto_response(reviews)
-st.write(reply)
+        Reply=gen_auto_response(reviews)
+        st.write(reply)
